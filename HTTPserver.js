3037a33315e95d0f82e0dee8352565912bc815ce
umutguder <mennanguder@gmail.com>
2022-06-22 16:55:29 +0300

    Update HTTPserver.js

--- HTTPserver.js
+++ HTTPserver.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const httpserver = http.createServer();
+httpserver.on("request", (req,res) => res.end("OK"))
 httpserver.on("listening", () => {
-    console.log(`Listening on port `)
+    console.log(`Listening on port ${httpserver.address().port}`)
 })
 
 httpserver.listen();
